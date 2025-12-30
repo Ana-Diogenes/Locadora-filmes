d32c91332590a2f08db45a1ef5e6f0eed82b0bd1
Ana Clara Oliveira Diógenes <anaclaradiogenes14@hotmail.com>
2025-12-30 20:16:48 -0300

    funcao inserir/mostrar filmes

---
+++ lib.py
@@ -0,0 +1,23 @@
+import csv
+
+def mostrar_filmes():
+    with open('filmes.csv',"r") as filmes:
+        lista_filmes = csv.reader(filmes, delimiter=",")
+        for i,linha in enumerate(lista_filmes):
+            if i!=0 and linha[4]=="Disponivel":
+                print(i,':',linha[0])
+            elif i!=0 and linha[4]!= "Disponivel":
+                print(i,':',linha[0],'(alugado)')
+
+
+titulo = input('Informe o titulo do filme: ')
+classificacao = input('Informe a classificação indicativa do filme: ')
+genero = input('Informe o genero do filme: ')
+sinopse = input ('Informe a sinopse do filme: ')
+
+def inserir_filme(titulo, classificacao, genero, sinopse):
+    mensagem = '\n'+ titulo +','+ classificacao +',' + genero.lower() +',' + sinopse + ',disponivel'
+    with open('filmes.csv',"a") as filmes:
+        filmes.write(mensagem)
+
+inserir_filme(titulo,classificacao,genero,sinopse)
