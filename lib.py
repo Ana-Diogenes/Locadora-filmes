3339d8dc996aacf3c27078985b2451b5436230b4
Ana Clara Oliveira Diógenes <anaclaradiogenes14@hotmail.com>
2026-01-01 14:00:00 -0300

    inputs na função inserir

--- lib.py
+++ lib.py
@@ -4,9 +4,9 @@ def mostrar_filmes():
     with open('filmes.csv',"r") as filmes:
         lista_filmes = csv.reader(filmes, delimiter=",")
         for i,linha in enumerate(lista_filmes):
-            if i!=0 and linha[4]=="Disponivel":
+            if i!=0 and linha[4]=="disponivel":
                 print(i,':',linha[0])
-            elif i!=0 and linha[4]!= "Disponivel":
+            elif i!=0 and linha[4]!= "disponivel":
                 print(i,':',linha[0],'(alugado)')
 
 
@@ -15,9 +15,12 @@ classificacao = input('Informe a classificação indicativa do filme: ')
 genero = input('Informe o genero do filme: ')
 sinopse = input ('Informe a sinopse do filme: ')
 
-def inserir_filme(titulo, classificacao, genero, sinopse):
+def inserir_filme():
+    titulo = input('Informe o titulo do filme: ')
+    classificacao = input('Informe a classificação indicativa do filme: ')
+    genero = input('Informe o genero do filme: ')
+    sinopse = input ('Informe a sinopse do filme: ')
     mensagem = '\n'+ titulo +','+ classificacao +',' + genero.lower() +',' + sinopse + ',disponivel'
     with open('filmes.csv',"a") as filmes:
         filmes.write(mensagem)
 
-inserir_filme(titulo,classificacao,genero,sinopse)
