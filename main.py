78f61fa5b3e6660d9b0be5e56c516256636b6040
Ana Clara Oliveira Diógenes <anaclaradiogenes14@hotmail.com>
2026-01-04 17:40:56 -0300

    cores novas

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ locadora = Tk()
 locadora.title('Locadora de filmes :)')
 locadora.geometry('800x500')
 locadora.resizable(False,False)
-locadora.configure(background="#fff3ac")
+locadora.configure(background="#2d3250")
 modo = 'inicio'
 
 def modo_busca():
@@ -60,59 +60,59 @@ def tela():
 
     limpar_tela(locadora)
     if modo == 'inicio':
-        h1 = Label(locadora, text='Seja bem vindo a locadora!', background='#fff3ac', font=('Arial',32,"bold"))
+        h1 = Label(locadora, text='Seja bem vindo a locadora!', background='#2d3250', font=('Arial',32,"bold"),foreground='#ffffff')
         h1.place(relx=0.5,y=30, anchor='center')
-        h2 = Label(locadora, text='O que deseja fazer?', background='#fff3ac', font=('Arial',20))
+        h2 = Label(locadora, text='O que deseja fazer?', background='#2d3250', font=('Arial',20), foreground="#ffffff")
         h2.place(relx=0.5,y=75,anchor='center')
 
-        buscar = Button(locadora, text='Buscar filme', command=modo_busca, font=('Arial',12))
+        buscar = Button(locadora, text='Buscar filme', command=modo_busca, font=('Arial',12), foreground='#2d3250', background='#ffcbcf')
         buscar.place(relx=0.1,y=140, width=120, height=50, anchor='center')
-        locar = Button(locadora, text='Locar filme', command=modo_locar, font=('Arial',12))
+        locar = Button(locadora, text='Locar filme', command=modo_locar, font=('Arial',12), foreground='#2d3250', background='#ffcbcf')
         locar.place(relx=0.3, y=140, width=120, height=50, anchor='center')
-        devolver = Button(locadora, text='Devolver filme', command=modo_devolver, font=('Arial',12))
+        devolver = Button(locadora, text='Devolver filme', command=modo_devolver, font=('Arial',12), foreground='#2d3250', background='#ffcbcf')
         devolver.place(relx=0.5, y=140, width=120, height=50, anchor='center')
-        cadastrar = Button(locadora, text='Cadastrar filme', command=modo_cadastrar, font=('Arial',12))
+        cadastrar = Button(locadora, text='Cadastrar filme', command=modo_cadastrar, font=('Arial',12), foreground='#2d3250', background='#ffcbcf')
         cadastrar.place(relx=0.7, y=140, width=120, height=50, anchor='center')
-        remover = Button(locadora, text='Remover filme', command=modo_remover, font=('Arial',12))
+        remover = Button(locadora, text='Remover filme', command=modo_remover, font=('Arial',12), foreground='#2d3250', background='#ffcbcf')
         remover.place(relx=0.9, y=140, width=120, height=50, anchor='center')
 
-        catalogo = Label(locadora, text='Catalogo:',background='#fff3ac', font=('Arial',16))
+        catalogo = Label(locadora, text='Catalogo:',background='#2d3250', font=('Arial',16), foreground='#ffffff')
         catalogo.place(relx=0.5,y=190,anchor='center')
-        filmes = ScrolledText(locadora, width=70, height=12, font=('Arial',12))
+        filmes = ScrolledText(locadora, width=70, height=12, font=('Arial',12), background = "#606378", foreground = '#ffffff')
         filmes.place(relx=0.5, y=330,anchor='center')
         filmes.insert(INSERT,mostrar_filmes())
         filmes.configure(state='disabled')
 
-        recomendacao = Button(locadora,text='Recomendação personalizada!', command=modo_recomendar,font=('Arial',12))
+        recomendacao = Button(locadora,text='Recomendação personalizada!', command=modo_recomendar,font=('Arial',12), foreground='#2d3250',background='#ffcbcf')
         recomendacao.place(relx=0.5, y= 470, anchor='center')
 
     elif modo == 'busca':
-        h1 = Label(locadora, text='Qual filme deseja buscar?', background='#fff3ac', font=('Arial',32,"bold"))
+        h1 = Label(locadora, text='Qual filme deseja buscar?', background='#2d3250', font=('Arial',32,"bold"), foreground='#ffffff')
         h1.place(relx=0.5,y=30, anchor='center')
         texto = StringVar()
-        info = Entry(locadora,textvariable=texto, font=('Arial',12))
+        info = Entry(locadora,textvariable=texto, font=('Arial',12), background = "#606378", foreground = '#ffffff')
         info.place(relx=0.5, y=90, anchor='center', width=300, height=30)
-        enviar_info = Button(locadora, text='Procurar', font=('Arial',12), width=7, command=fazer_busca)
+        enviar_info = Button(locadora, text='Procurar', font=('Arial',12), width=7, command=fazer_busca, foreground='#2d3250', background='#ffcbcf')
         enviar_info.place(relx=0.45,y=130, anchor='center')
-        voltar = Button(locadora, text='Inicio', font=('Arial',12), command=modo_inicio, width=7)
+        voltar = Button(locadora, text='Inicio', font=('Arial',12), command=modo_inicio, width=7, foreground='#2d3250', background='#ffcbcf')
         voltar.place(relx=0.55, y=130, anchor='center' )
 
-        resposta = Label(locadora, text='',font=('Arial',12), background='#fff3ac', wraplength=400)
+        resposta = Label(locadora, text='',font=('Arial',12), background='#2d3250', wraplength=400, foreground='#ffffff')
         resposta.place(relx=0.5,y=200, anchor=N)
 
 
     elif modo == 'locar':
-        h1 = Label(locadora, text='Qual filme deseja locar?', background='#fff3ac', font=('Arial',32,"bold"))
+        h1 = Label(locadora, text='Qual filme deseja locar?', background='#2d3250', font=('Arial',32,"bold"), foreground='#ffffff')
         h1.place(relx=0.5,y=30, anchor='center')
         texto = StringVar()
-        info = Entry(locadora,textvariable=texto, font=('Arial',12))
+        info = Entry(locadora,textvariable=texto, font=('Arial',12), background = "#606378", foreground = '#ffffff')
         info.place(relx=0.5, y=90, anchor='center', width=300, height=30)
-        enviar_info = Button(locadora, text='Locar', font=('Arial',12), width=7, command=fazer_locacao)
+        enviar_info = Button(locadora, text='Locar', font=('Arial',12), width=7, command=fazer_locacao, foreground='#2d3250', background='#ffcbcf')
         enviar_info.place(relx=0.45,y=130, anchor='center')
-        voltar = Button(locadora, text='Inicio', font=('Arial',12), command=modo_inicio, width=7)
+        voltar = Button(locadora, text='Inicio', font=('Arial',12), command=modo_inicio, width=7, foreground='#2d3250', background='#ffcbcf')
         voltar.place(relx=0.55, y=130, anchor='center' )
 
-        resposta = Label(locadora, text='',font=('Arial',12), background='#fff3ac', wraplength=400)
+        resposta = Label(locadora, text='',font=('Arial',12), background='#2d3250', wraplength=400, foreground='#ffffff')
         resposta.place(relx=0.5,y=200, anchor=N)
 
     elif modo == 'devolver':
