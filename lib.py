7c8e5900c16481693dfc7f43829c3ae4e390aefa
Ana Clara Oliveira Diógenes <anaclaradiogenes14@hotmail.com>
2026-01-05 17:48:15 -0300

    Comentarios

--- lib.py
+++ lib.py
@@ -2,38 +2,38 @@ import csv
 from tkinter import *
 
 
-def mostrar_filmes():
-    catalogo = ''
-    with open('filmes.csv',"r") as filmes:
-        lista_filmes = csv.reader(filmes, delimiter=",")
+def mostrar_filmes(): #Função que mostra todos os filmes
+    catalogo = '' #Variavel para armazenar o catalogo
+    with open('filmes.csv',"r") as filmes: #Abro o arquivo csv no modo leitura
+        lista_filmes = csv.reader(filmes, delimiter=",") #Cria um objeto csv em que cada linha é uma lista
         for i,linha in enumerate(lista_filmes):
-            if i!=0 and linha[4]=="disponivel":
+            if i!=0 and linha[4]=="disponivel": #Confere que o item não é o primeiro e se está disponivel
                 catalogo += str(i) + ': ' + linha[0] + '\n'
-            elif i!=0 and linha[4]!= "disponivel":
+            elif i!=0 and linha[4]!= "disponivel": #Confere que o item não é o primeiro e se está alugado
                 catalogo += str(i) + ': ' + linha[0] +' (alugado)'+ '\n'
     return catalogo
 
-def inserir_filme(titulo, classificacao, genero, sinopse):
-    if titulo =='' or classificacao =='Classificacoes' or genero =='Generos' or sinopse =='':
+def inserir_filme(titulo, classificacao, genero, sinopse): #Função que adiciona filmes novos
+    if titulo =='' or classificacao =='Classificacoes' or genero =='Generos' or sinopse =='': #Verifica se algum dos campos não foi preenchido
         return 'Preencha os campos em branco'
     mensagem = '\n'+ titulo +','+ classificacao +',' + genero.lower() +',' + sinopse + ',disponivel'
-    with open('filmes.csv',"a") as filmes:
-        filmes.write(mensagem)
+    with open('filmes.csv',"a") as filmes: #Abro o arquivo csv no modo append (adicionar informações)
+        filmes.write(mensagem) #Adiciono o novo filme
     return 'Filme inserido com sucesso'
 
-def excluir_filme(excluido):
+def excluir_filme(excluido): #Função que apaga filmes
     lista_nova = []
     achou = False
     with open('filmes.csv',"r") as filmes:
         lista_filmes = csv.reader(filmes, delimiter=",")
         for linha in lista_filmes:
-            if (linha [0]).lower() != (excluido).lower():
+            if (linha [0]).lower() != (excluido).lower(): #Se o filme que está sendo percorrido tiver um titulo diferente do excluido, ele é adicionado a lista nova
                 lista_nova.append(linha)
-            elif (linha [0]).lower() == (excluido).lower():
+            elif (linha [0]).lower() == (excluido).lower(): #Se o titulo for igual ao excluido, ele não é adicionado
                 achou = True
-    if achou == False:
+    if achou == False: #Se o filme não for encontrado, não há necessidade de reescrevor o arquivo, retorna uma mensagem de erro
         return 'O filme que deseja excluir não foi encontrado'
-    with open ('filmes.csv','w') as filmes:
+    with open ('filmes.csv','w') as filmes: #Reescreve o arquivo linha por linha usando os filmes da lista_nova (que não tem o filme excluido)
         for i,linha in enumerate(lista_nova):
             titulo = linha[0]
             classificacao = linha[1]
@@ -47,48 +47,47 @@ def excluir_filme(excluido):
                 filmes.write('\n'+mensagem)
     return 'Filme deletado com sucesso!'
 
-def buscar_filme(buscado):
+def buscar_filme(buscado): #Função que procura filmes
     resposta = ''
     with open ('filmes.csv','r') as filmes:
         lista_filmes = csv.reader(filmes, delimiter=',')
         encontrado = False
         for linha in lista_filmes:
-            if (linha[0]).lower() == buscado.lower():
+            if (linha[0]).lower() == buscado.lower(): #Procura o filme no arquivo
                 encontrado = True
                 resposta += ('Titulo: '+ str(linha[0])+'\n')
                 resposta += ('Classificacao indicativa: '+(linha[1])+'\n')
                 resposta += ('Genero: '+ str(linha[2])+'\n')
                 resposta += ('Sinopse: '+ str(linha[3])+'\n')
-                if linha[4]=='disponivel':
+                if linha[4]=='disponivel': #Confere se o filme está disponivel
                     resposta += ('O filme está disponivel para aluguel!')
                 else:
                     resposta += ('O filme não está disponivel para aluguel')
-        if encontrado == False:
-            resposta = ('Não encontramos o filme que deseja')
+        if encontrado == False: 
+            resposta = ('Não encontramos o filme que deseja') #Mensagem caso o filme não seja encontrado
     return resposta
 
-def locar_filme(locado):
+def locar_filme(locado): #Função que loca os filme
     resposta = ''
     achou = False
     with open ('filmes.csv','r') as filmes:
         lista_filmes = csv.reader(filmes,delimiter=',')
         lista_nova =[]
         for linha in lista_filmes:
-            if (linha[0]).lower() == locado.lower() and linha[4]== 'disponivel':
-                filme = [linha[0],linha[1],linha[2],linha[3],'alugado']
+            if (linha[0]).lower() == locado.lower() and linha[4]== 'disponivel': #Confere se achou o filme e se está disponivel
+                filme = [linha[0],linha[1],linha[2],linha[3],'alugado'] #O filme agora é adicionado na lista com o status 'alugado'
                 lista_nova.append(filme)
                 resposta = ('O filme foi locado com sucesso! Não se esqueça de devolver!')
                 achou = True
             elif (linha[0]).lower() == locado.lower() and linha[4]!= 'disponivel':
                 achou = True
-                return ('O filme que você deseja não está disponivel!')
-                
+                return ('O filme que você deseja não está disponivel!')  #Mensagem caso o filme locado não esteja disponivel
             else:
                 lista_nova.append(linha)
         if achou == False:
-            return 'Não achamos o filme que deseja locar'
+            return 'Não achamos o filme que deseja locar' #Mensagem caso o filme não seja encontrado
         with open ('filmes.csv','w') as filmes:
-            for i,linha in enumerate(lista_nova):
+            for i,linha in enumerate(lista_nova): #Reescreve o arquivo com o filme locado
                 titulo = linha[0]
                 classificacao = linha[1]
                 genero = linha[2]
@@ -101,7 +100,7 @@ def locar_filme(locado):
                     filmes.write('\n'+mensagem)
     return resposta
 
-def devolver_filme(devolvido):
+def devolver_filme(devolvido): #Função para devolver filmes
     achou = False
     resposta = ''
     with open ('filmes.csv','r') as filmes:
@@ -109,7 +108,7 @@ def devolver_filme(devolvido):
         lista_nova =[]
         for linha in lista_filmes:
             if (linha[0]).lower() == devolvido.lower() and linha[4]== 'alugado':
-                filme = [linha[0],linha[1],linha[2],linha[3],'disponivel']
+                filme = [linha[0],linha[1],linha[2],linha[3],'disponivel'] 
                 lista_nova.append(filme)
                 resposta = ('O filme foi devolvido com sucesso!')
                 achou = True
@@ -136,19 +135,19 @@ def devolver_filme(devolvido):
                     filmes.write('\n'+mensagem)
     return resposta
 
-def indicação_personalizada(classificacao, genero):
-    quantidade_filmes = 0
+def indicação_personalizada(classificacao, genero): #Função que faz recomendação
+    quantidade_filmes = 0 #Variavel para controlar a quantidade de filmes recomendados
     mensagem = 'Voce pode gostar do(s) filme(s):\n'
     with open ('filmes.csv','r') as filmes:
         lista_filmes = csv.reader(filmes,delimiter=',')
         for linha in lista_filmes:
-            if linha[1].lower() == classificacao.lower() and linha[2].lower()== genero.lower() and linha[4]=='disponivel' and quantidade_filmes<2:
-                mensagem += ('\ntitulo: '+str(linha[0])+'\nsinopse: '+ str(linha[3])+'\n')
+            if linha[1].lower() == classificacao.lower() and linha[2].lower()== genero.lower() and linha[4]=='disponivel' and quantidade_filmes<2: #Confere se o filme é compativel com as caracteristicas desejadas, se está disponivel e se a quantidade limite não foi atingida
+                mensagem += ('\ntitulo: '+str(linha[0])+'\nsinopse: '+ str(linha[3])+'\n') #adiciona o filme a mensagem
                 quantidade_filmes+=1
     if mensagem == 'Voce pode gostar do(s) filme(s):\n':
-        return 'Não encontramos o filme com as caracteristicas que deseja'
+        return 'Não encontramos o filme com as caracteristicas que deseja' #Resposta caso nenhum filme seja adicionado a mensagem
     return mensagem
 
-def limpar_tela(locadora):
-    for widget in locadora.winfo_children():
-        widget.place_forget()
+def limpar_tela(locadora): #Função para limpar tela
+    for widget in locadora.winfo_children(): #Percorre todos os widgets na tela
+        widget.place_forget() #Remove o posicionamento desses itens
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from lib import *
 from tkinter import *
 from tkinter.scrolledtext import *
 
+#Configurações iniciais da interface
 locadora = Tk()
 locadora.title('Locadora de filmes :)')
 locadora.geometry('800x500')
@@ -9,58 +10,51 @@ locadora.resizable(False,False)
 locadora.configure(background="#2d3250")
 modo = 'inicio'
 
+#Funções para mudar o modo da tela:
 def modo_busca():
     global modo
-    modo = 'busca'
-    tela()
-
+    modo = 'busca' #Mudo o modo
+    tela() #Chamo a função tela pra ela renderizar a tela de novo
 def modo_locar():
     global modo
     modo = 'locar'
     tela()
-
 def modo_devolver():
     global modo
     modo = 'devolver'
     tela()
-
 def modo_cadastrar():
     global modo
     modo = 'cadastrar'
     tela()
-
 def modo_remover():
     global modo
     modo = 'remover'
     tela()
-
 def modo_recomendar():
     global modo
     modo = 'recomendar'
     tela()
-
 def modo_inicio():
     global modo
     modo = 'inicio'
     tela()
 
-def tela():
+def tela(): #Função principal do projeto
     
+    #Funções para executar as funções nos botões e mostrar a resposta
     def fazer_busca():
-        buscado = texto.get()
-        resultado = buscar_filme(buscado)
-        resposta ['text'] = resultado
-
+        buscado = texto.get() #Pego o texto que o usuario digitou
+        resultado = buscar_filme(buscado) #Executo a função desejada usando o texto do usuario como parametro
+        resposta ['text'] = resultado #Pego o resultado da função e faço ele aparecer no texto do label resposta
     def fazer_locacao():
         locado = texto.get()
         resultado = locar_filme(locado)
         resposta ['text'] = resultado
-
     def fazer_devolucao():
         devolvido = texto.get()
         resultado = devolver_filme(devolvido)
         resposta ['text'] = resultado
-
     def fazer_cadastro():
         titulo_cadastro = titulo.get()
         classificacao_cadastro = classificacao.get()
@@ -68,26 +62,27 @@ def tela():
         sinopse_cadastro = sinopse.get()
         resultado = inserir_filme(titulo_cadastro, classificacao_cadastro,genero_cadastro,sinopse_cadastro)
         resposta ['text']=resultado
-
     def fazer_remocao():
         removido = texto.get()
         resultado = excluir_filme(removido)
         resposta['text']= resultado
-
     def fazer_recomendação():
         classificacao_rec = classificacao.get()
         genero_rec = genero.get()
         resultado = indicação_personalizada(classificacao_rec, genero_rec)
         resposta['text']= resultado
 
+    limpar_tela(locadora) #Limpo a tela antes de começar para apagar as informações da tela anterior
 
-    limpar_tela(locadora)
-    if modo == 'inicio':
-        h1 = Label(locadora, text='Seja bem vindo a locadora!', background='#2d3250', font=('Arial',32,"bold"),foreground='#ffffff')
-        h1.place(relx=0.5,y=30, anchor='center')
+    if modo == 'inicio': #Tela inicial
+
+        #Titulo e subtitulo da pagina
+        h1 = Label(locadora, text='Seja bem vindo a locadora!', background='#2d3250', font=('Arial',32,"bold"),foreground='#ffffff') #Parametros do item
+        h1.place(relx=0.5,y=30, anchor='center') #Posicionando o item
         h2 = Label(locadora, text='O que deseja fazer?', background='#2d3250', font=('Arial',20), foreground="#ffffff")
         h2.place(relx=0.5,y=75,anchor='center')
 
+        #Botões para trocar de tela e fazer alguma das funções listadas
         buscar = Button(locadora, text='Buscar filme', command=modo_busca, font=('Arial',12), foreground='#2d3250', background='#ffcbcf')
         buscar.place(relx=0.1,y=140, width=120, height=50, anchor='center')
         locar = Button(locadora, text='Locar filme', command=modo_locar, font=('Arial',12), foreground='#2d3250', background='#ffcbcf')
@@ -98,35 +93,37 @@ def tela():
         cadastrar.place(relx=0.7, y=140, width=120, height=50, anchor='center')
         remover = Button(locadora, text='Remover filme', command=modo_remover, font=('Arial',12), foreground='#2d3250', background='#ffcbcf')
         remover.place(relx=0.9, y=140, width=120, height=50, anchor='center')
-
-        catalogo = Label(locadora, text='Catalogo:',background='#2d3250', font=('Arial',16), foreground='#ffffff')
-        catalogo.place(relx=0.5,y=190,anchor='center')
-        filmes = ScrolledText(locadora, width=70, height=12, font=('Arial',12), background = "#606378", foreground = '#ffffff')
-        filmes.place(relx=0.5, y=330,anchor='center')
-        filmes.insert(INSERT,mostrar_filmes())
-        filmes.configure(state='disabled')
-
         recomendacao = Button(locadora,text='Recomendação personalizada!', command=modo_recomendar,font=('Arial',12), foreground='#2d3250',background='#ffcbcf')
         recomendacao.place(relx=0.5, y= 470, anchor='center')
 
-    elif modo == 'busca':
+        #Mostrando o catalogo de filmes
+        catalogo = Label(locadora, text='Catalogo:',background='#2d3250', font=('Arial',16), foreground='#ffffff')
+        catalogo.place(relx=0.5,y=190,anchor='center')
+        filmes = ScrolledText(locadora, width=70, height=12, font=('Arial',12), background = "#606378", foreground = '#ffffff') #Tetxo com barra de rolagem (por padrão, deve ser digitado pelo usuario)
+        filmes.place(relx=0.5, y=330,anchor='center')
+        filmes.insert(INSERT,mostrar_filmes()) #Coloco a função mostrar_filmes para executar dentro do campo de texto
+        filmes.configure(state='disabled') #Bloqueio o usuario de mudar o texto 
+
+    elif modo == 'busca': #Tela para buscar filmes
         h1 = Label(locadora, text='Qual filme deseja buscar?', background='#2d3250', font=('Arial',32,"bold"), foreground='#ffffff')
         h1.place(relx=0.5,y=110, anchor='center')
-        texto = StringVar()
-        info = Entry(locadora,textvariable=texto, font=('Arial',12), background = "#606378", foreground = '#ffffff')
+
+        texto = StringVar() #Variavel para armazenar texto dos inputs
+        info = Entry(locadora,textvariable=texto, font=('Arial',12), background = "#606378", foreground = '#ffffff') #Input
         info.place(relx=0.5, y=170, anchor='center', width=300, height=30)
-        enviar_info = Button(locadora, text='Procurar', font=('Arial',12), width=7, command=fazer_busca, foreground='#2d3250', background='#ffcbcf')
+        enviar_info = Button(locadora, text='Procurar', font=('Arial',12), width=7, command=fazer_busca, foreground='#2d3250', background='#ffcbcf') #Chamo a função que faz a busca quando clico nesse botão
         enviar_info.place(relx=0.45,y=210, anchor='center')
-        voltar = Button(locadora, text='Inicio', font=('Arial',12), command=modo_inicio, width=7, foreground='#2d3250', background='#ffcbcf')
+        voltar = Button(locadora, text='Inicio', font=('Arial',12), command=modo_inicio, width=7, foreground='#2d3250', background='#ffcbcf') #Botão para voltar a tela inicial
         voltar.place(relx=0.55, y=210, anchor='center' )
 
-        resposta = Label(locadora, text='',font=('Arial',12), background='#2d3250', wraplength=400, foreground='#ffffff')
+        resposta = Label(locadora, text='',font=('Arial',12), background='#2d3250', wraplength=400, foreground='#ffffff') #Espaço onde o resultado da função vai aparecer quando ela for chamada
         resposta.place(relx=0.5,y=270, anchor=N)
 
 
-    elif modo == 'locar':
+    elif modo == 'locar': #Tela para locar filmes
         h1 = Label(locadora, text='Qual filme deseja locar?', background='#2d3250', font=('Arial',32,"bold"), foreground='#ffffff')
         h1.place(relx=0.5,y=110, anchor='center')
+
         texto = StringVar()
         info = Entry(locadora,textvariable=texto, font=('Arial',12), background = "#606378", foreground = '#ffffff')
         info.place(relx=0.5, y=170, anchor='center', width=300, height=30)
@@ -138,9 +135,10 @@ def tela():
         resposta = Label(locadora, text='',font=('Arial',12), background='#2d3250', wraplength=400, foreground='#ffffff')
         resposta.place(relx=0.5,y=270, anchor=N)
 
-    elif modo == 'devolver':
+    elif modo == 'devolver': #Tela para devolver filmes
         h1 = Label(locadora, text='Qual filme deseja devolver', background='#2d3250', font=('Arial',32,"bold"), foreground='#ffffff')
         h1.place(relx=0.5,y=110, anchor='center')
+
         texto = StringVar()
         info = Entry(locadora,textvariable=texto, font=('Arial',12), background = "#606378", foreground = '#ffffff')
         info.place(relx=0.5, y=170, anchor='center', width=300, height=30)
@@ -152,7 +150,7 @@ def tela():
         resposta = Label(locadora, text='',font=('Arial',12), background='#2d3250', wraplength=400, foreground='#ffffff')
         resposta.place(relx=0.5,y=270, anchor=N)
 
-    elif modo == 'cadastrar':
+    elif modo == 'cadastrar': #Tela para cadastrar filmes
         h1 = Label(locadora, text='Adicione as informações do \n novo filme', background='#2d3250', font=('Arial',32,"bold"), foreground='#ffffff')
         h1.place(relx=0.5,y=50, anchor='center')
 
@@ -164,10 +162,10 @@ def tela():
 
         label_c = Label (locadora, text='Classificacao indicativa:', background='#2d3250', font=('Arial',12), foreground='#ffffff')
         label_c.place(x=100, y=180)
-        classificacoes = ["L", "10", "12", "14","16","18"]
-        classificacao = StringVar()
-        classificacao.set("Classificacoes")
-        menu_c = OptionMenu(locadora, classificacao, *classificacoes)
+        classificacoes = ["L", "10", "12", "14","16","18"] #Lista com as opçoes disponiveis para o menu
+        classificacao = StringVar() #Variavel para guardar a opção escolhida
+        classificacao.set("Classificacoes") #Aparece enquanto nenhuma opção for escolhida
+        menu_c = OptionMenu(locadora, classificacao, *classificacoes) #Crio o menu
         menu_c.place(x=100, y=210)
 
         label_g = Label (locadora, text='Genero:', background='#2d3250', font=('Arial',12), foreground='#ffffff')
@@ -191,7 +189,8 @@ def tela():
 
         resposta = Label(locadora, text='',font=('Arial',12), background='#2d3250', wraplength=400, foreground='#ffffff')
         resposta.place(relx=0.5,y=440, anchor=N)
-    elif modo == 'remover':
+
+    elif modo == 'remover': #Tela para remover filmes
         h1 = Label(locadora, text='Qual filme deseja remover?', background='#2d3250', font=('Arial',32,"bold"), foreground='#ffffff')
         h1.place(relx=0.5,y=110, anchor='center')
         texto = StringVar()
@@ -205,7 +204,7 @@ def tela():
         resposta = Label(locadora, text='',font=('Arial',12), background='#2d3250', wraplength=400, foreground='#ffffff')
         resposta.place(relx=0.5,y=270, anchor=N)    
 
-    elif modo == 'recomendar':
+    elif modo == 'recomendar': #Tela para receber uma recomendação
         h1 = Label(locadora, text='Que tipo de filme você gosta?', background='#2d3250', font=('Arial',32,"bold"), foreground='#ffffff')
         h1.place(relx=0.5,y=50, anchor='center')
 
@@ -234,5 +233,6 @@ def tela():
         resposta.place(relx=0.5,y=220, anchor=N)
         
 
-tela()
-locadora.mainloop()
+tela() #Chamo a função principal
+
+locadora.mainloop() #Função para tela ficar aparecendo
